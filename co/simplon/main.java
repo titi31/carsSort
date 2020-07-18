d3287c254ab4b05df003c158710c948f106dc51b
Mr.Robot <classic.graal095@gmail.com>
2020-07-18 17:20:46 +0000

    exo trie

--- co/simplon/main.java
+++ co/simplon/main.java
@@ -1,35 +1,51 @@
 package co.simplon;
-import java.util.*;
+import java.util.ArrayList;
 
 class Main {
-    private Cars[] tab={new Cars("car","BMW", "blue"),
-new Cars("car", "BMW", "red"),
-new Cars("car", "BMW 2", "blue"),
-new Cars("truck","MERCEDES","blue"),
-new Cars("truck", "MERCEDES", "red"),
-new Cars( "truck", "MERCEDES","green"),
-new Cars("truck","HYUNDAI", "Blue"),
-new Cars("boat","yachts","Blue"),
-new Cars("boat", "yachts", "Blue"),
-new Cars("boat", "yachts","yellow")};
+    private static ArrayList<Cars> tab=new ArrayList<Cars>();
+    
+
 
     public static void main(String[] args) {
-        
+        tab.add(new Cars("car","BMW", "blue"));
+        tab.add(new Cars("car", "BMW", "red"));
+        tab.add(new Cars("car", "BMW 2", "blue"));
+        tab.add(new Cars("truck","MERCEDES","blue"));
+        tab.add(new Cars("truck", "MERCEDES", "red"));
+        tab.add(new Cars( "truck", "MERCEDES","green"));
+        tab.add(new Cars("truck","HYUNDAI", "Blue"));
+        tab.add(new Cars("boat","yachts","Blue"));
+        tab.add(new Cars("boat", "yachts", "Blue"));
+        tab.add(new Cars("boat", "yachts","yellow"));
+       
+         trie(tab);
 
-       /* JSONParser jsonP = new JSONParser();
+      
+    }
+    public static void trie(ArrayList<Cars> tab){
+        while(tab.size()>0){
+         String cat=tab.get(0).getCategoryName();
+        String color=tab.get(0).getColor();
+        ArrayList<String> tabCat=new ArrayList<String>();
+        ArrayList<Cars> num=new ArrayList<Cars>();
+        for(int car=0;car<tab.size();car++){
+           if(cat.contentEquals(tab.get(car).getCategoryName()) && color.contentEquals(tab.get(car).getColor())){
+                tabCat.add(tab.get(car).getName());
+                System.out.println(car);
+                num.add(tab.get(car));
+               
+               
+           }
+           
+        }
+        for(int i=0;i<num.size();i++){
+         
+         tab.remove(tab.indexOf(num.get(i)));
 
-  try {
-         JSONObject jsonO = (JSONObject)jsonP.parse(new FileReader("/workspace/carsSort/cars.json"));
-        System.out.println(jsonO);
-         /*String name = (String) jsonO.get("name");
-         String age = (String) jsonO.get("age");
-         String address = (String) jsonO.get("address");
-         System.out.println("Name :"+ name);
-         System.out.println("Age: "+ age);
-         System.out.println("Address: "+ address);
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();
-        
-    }*/
+        }
+      
+        System.out.println("Les voitures de categorie "+cat+" et de couleur "+color+" sont : "+tabCat);
+       
+    }
     }
 }
